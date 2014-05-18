6907e4ee3c15f304200ae7250c007a65eb658e71
David Hays <Dhays90@gmail.com>
2014-05-18 10:57:53 -0500

    t6: update fingerprint

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/t6/full_t6.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t6ul BUILD_ID=JSS15J BUILD_FINGERPRINT="htc/htc_europe/t6ul:4.3/JSS15J/259366.2:user/release-keys" PRIVATE_BUILD_DESC="1.19.401.2 CL259366 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_europe BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/htc_europe/t6ul:4.4.2/KOT49H/338980.1:user/release-keys" PRIVATE_BUILD_DESC="3.12.401.1 CL338980 release-keys"
 
 PRODUCT_NAME := cm_t6
 PRODUCT_DEVICE := t6
