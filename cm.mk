b099dc8f069c5dac8a18c3c7cf9887016c2a06fd
David Hays <dhays90@gmail.com>
2015-06-21 08:18:20 -0500

    t6: Update fingerprint
    Change-Id: I1af0c6da0c614ef953afeeab272b8d851a1556e4

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/t6/full_t6.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_europe BUILD_ID=KOT49H BUILD_FINGERPRINT="htc/htc_europe/t6ul:4.4.2/KOT49H/338980.1:user/release-keys" PRIVATE_BUILD_DESC="3.12.401.1 CL338980 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_europe BUILD_ID=LRX22G BUILD_FINGERPRINT="htc/htc_europe/t6ul:5.0.2/LRX22G/525693.7:user/release-keys" PRIVATE_BUILD_DESC="4.13.401.7 CL525693 release-keys"
 
 PRODUCT_NAME := cm_t6
 PRODUCT_DEVICE := t6
