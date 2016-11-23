a18198f840c894c865cc875820690c9d9365e9f4
David Hays <dhays90@gmail.com>
2016-11-23 08:24:05 -0600

    t6: Remove enhanced NFC feature
    Change-Id: Ice86db7c91322834c8950c7fb3ac9d74afdd70dd

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/htc/t6/full_t6.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_europe BUILD_ID=LRX22G BUILD_FINGERPRINT="htc/htc_europe/t6ul:5.0.2/LRX22G/525693.7:user/release-keys" PRIVATE_BUILD_DESC="4.13.401.7 CL525693 release-keys"
