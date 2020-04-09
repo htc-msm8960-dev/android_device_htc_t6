1c001e03c1c5710b79667f71a5f261e723fc2f37
Kevin F. Haggerty <haggertk@lineageos.org>
2020-04-08 19:22:18 -0600

    t6: Set shipping API level in the Lineage product makefile
    Change-Id: I1dc0862a93b3ee2da6673741899f295129f5297c

--- lineage_t6.mk
+++ lineage_t6.mk
@@ -16,6 +16,7 @@
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/build/target/product/product_launched_with_j_mr2.mk)
 
 $(call inherit-product, device/htc/t6/full_t6.mk)
 
